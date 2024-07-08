1c8a6e757a2a089b845d444f2dd03dd095879c5d
sanjit samar <sanjit1525@gmail.com>
2024-07-08 23:47:43 +0530

    commit

--- src/Cats/catsSaga.js
+++ src/Cats/catsSaga.js
@@ -1,17 +1,19 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-//import catsApi from "../services/catsApi";
+import catsApi from "../services/catsApi";
 import {getCatsSuccess} from './catsSlice';
 // call is to call the apis
 // put is used to allow call of actions
 // takeEvery to watch or trigger the function whenever action is called
 
 function* workGetCatsfetch(){
-    // const catsResponse = yield call(catsApi());
-    // yield put(getCatsSuccess(catsResponse));
-    const catsResponse = yield call(()=> fetch('https://api.thecatapi.com/v1/breeds'));
-    const response = yield catsResponse.json();
-    const formattedResponse = response.slice(0,10);
-    yield put(getCatsSuccess(formattedResponse));
+    const catsResponse = yield call(()=>catsApi());
+    yield put(getCatsSuccess(catsResponse));
+
+    
+    // const catsResponse = yield call(()=> fetch('https://api.thecatapi.com/v1/breeds'));
+    // const response = yield catsResponse.json();
+    // const formattedResponse = response.slice(0,10);
+    //yield put(getCatsSuccess(formattedResponse));
 }
 
 function* catsSaga(){
--- src/services/catsApi.js
+++ src/services/catsApi.js
@@ -3,7 +3,7 @@
 const catsApi = () => {
     return fetch('https://api.thecatapi.com/v1/breeds')
     .then( res => res.json())
-    .then(value => console.log(value.slice(0,10)));
+    .then(value => value.slice(0,10));
 }
 
 export default catsApi;
